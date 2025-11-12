334ef7d4ee8f0658030453590ada2578f72569ee
rarathore02 <rashikarathore.rr02@gmail.com>
2025-11-12 10:33:24 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5, key = 30;
+    int result = binarySearch(arr, n, key);
+    (result == -1) ? cout << "Not Found" : cout << "Found at index " << result;
+    return 0;
+}
