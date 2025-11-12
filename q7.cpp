a4c7b59f72c751c836a7994df76bec062de5b231
rarathore02 <rashikarathore.rr02@gmail.com>
2025-11-12 10:38:25 +0530

    Create q7.cpp

---
+++ q7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 4, 5};
+    int n = 7;
+    set<int> s(arr, arr + n);
+    cout << "Total distinct elements: " << s.size();
+}
